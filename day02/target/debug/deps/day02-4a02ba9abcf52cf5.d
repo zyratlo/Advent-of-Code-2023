6b137010b6fe15c702c838a0c7892d697d09d441
Ryan Zhang <zyratlo@gmail.com>
2023-12-03 18:53:52 -0800

    day02 initial commit

---
+++ day02/target/debug/deps/day02-4a02ba9abcf52cf5.d
@@ -0,0 +1,5 @@
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\deps\libday02-4a02ba9abcf52cf5.rmeta: src\main.rs
+
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\deps\day02-4a02ba9abcf52cf5.d: src\main.rs
+
+src\main.rs:
---
+++ day02/target/debug/deps/day02-d86b025cba5fc613.d
@@ -0,0 +1,5 @@
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\deps\libday02-d86b025cba5fc613.rmeta: src\main.rs
+
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\deps\day02-d86b025cba5fc613.d: src\main.rs
+
+src\main.rs:
