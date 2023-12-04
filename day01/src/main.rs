8444e85d2d55a239f255f0d2e261cc9a0612031c
Ryan Zhang <zyratlo@gmail.com>
2023-12-03 18:04:07 -0800

    first puzzle complete

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs;
 
 fn main() {
@@ -6,8 +5,28 @@ fn main() {
 
     let input: String = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
+    
+    let mut total: u32 = 0;
 
     for line in input.lines() {
-        println!("{line}")
+        let mut calibration_value: u32 = 0;
+
+        for c in line.chars() {
+            if c.is_ascii_digit() {
+                calibration_value = (c as u32 - 48) * 10;
+                break;
+            }
+        }
+
+        for c in line.chars().rev() {
+            if c.is_ascii_digit() {
+                calibration_value += c as u32 - 48;
+                break;
+            }
+        }
+        
+        total += calibration_value;
     }
+
+    println!("{total}");
 }
