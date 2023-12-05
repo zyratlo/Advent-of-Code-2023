b0bcd39bc277e1c3504af3a8c2b1d49d56bbc679
Ryan Zhang <zyratlo@gmail.com>
2023-12-04 18:44:02 -0800

    part 2 solution

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,16 +7,48 @@ fn main() {
     let input: String = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
     
-    // let input: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-
     part1(&input);
+
+    part2(&input);
 }
+
+fn part2(input: &str) {
+    let mut total: u32 = 0;
+
+    for game in input.lines() {
+        let mut min_cubes:HashMap<&str, u32> = HashMap::new();
+
+        // Iterate through every reveal
+        for revealed in game[game.chars().position(|c| c == ':').unwrap()+1..game.len()].split(';') {
+            // Iterate through every revealed color
+            for revealed_cube in revealed.split(',') {
+                let revealed_cube: Vec<&str> = revealed_cube.trim().split(' ').collect();
+                let num: u32 = revealed_cube[0].parse().unwrap();
+                let color: &str = revealed_cube[1];
+                
+                // Check if the number of revealed for that color is more than our constraint
+                if !min_cubes.contains_key(color) || min_cubes[color] < num {
+                    min_cubes.remove(color);
+                    min_cubes.insert(color, num);
+                }
+            }
+        }
+        let mut temp_total: u32 = 1;
+        for num in min_cubes.values() {
+            temp_total *= num;
+        }
+        total += temp_total;
+    }
+
+    println!("Sum of powers: {total}");
+}
+
 
 
 fn part1(input: &str) {
     let mut total: u32 = 0;
 
-    let mut constraints = HashMap::new();
+    let mut constraints: HashMap<&str, u32> = HashMap::new();
     constraints.insert("red", 12);
     constraints.insert("green", 13);
     constraints.insert("blue", 14);
