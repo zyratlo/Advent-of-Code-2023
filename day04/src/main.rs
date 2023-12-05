80c8e824760c385b4cad4cdfb048a680f528fa57
Ryan Zhang <zyratlo@gmail.com>
2023-12-05 14:42:25 -0800

    day04 part 1 solution

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -6,12 +6,28 @@ fn main() {
     let input: String = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
     part1(&input);
 }
 
 
 fn part1(input: &str) {
-    let winning_nums: Vec<&str> = (&input[input.chars().position(|c| c == ':').unwrap()+1..input.chars().position(|c| c == '|').unwrap()]).trim().split_whitespace().collect();
-    let my_nums: Vec<&str> = (&input[input.chars().position(|c| c == '|').unwrap()+1..input.len()]).trim().split_whitespace().collect();
+    let mut total = 0;
+
+    for line in input.lines() {
+        let winning_nums: Vec<&str> = (&line[line.chars().position(|c| c == ':').unwrap()+1..line.chars().position(|c| c == '|').unwrap()]).trim().split_whitespace().collect();
+        let my_nums: Vec<&str> = (&line[line.chars().position(|c| c == '|').unwrap()+1..line.len()]).trim().split_whitespace().collect();
+
+        let mut matches: i32 = -1;
+        let base: u32 = 2;
+
+        for num in my_nums {
+            if winning_nums.contains(&num) {
+                matches += 1;
+            }
+        }
+        if matches > -1 {
+            total += base.pow(matches as u32);
+        }
+    }
+    println!("Total points: {total}");
 }
