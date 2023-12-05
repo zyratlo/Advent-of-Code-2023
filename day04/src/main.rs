d81366583f8d79d247574cf2bd4293c535f8dbf4
Ryan Zhang <zyratlo@gmail.com>
2023-12-05 15:01:59 -0800

    day04 part 2 solution

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -6,7 +6,34 @@ fn main() {
     let input: String = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    part1(&input);
+    // part1(&input);
+    
+    part2(&input);
+}
+
+fn part2(input: &str) {
+    let mut total = 0;
+    let mut copies: Vec<u32> = vec![1; input.lines().count()];
+
+    for (index, line) in input.lines().enumerate() {
+        let winning_nums: Vec<&str> = (&line[line.chars().position(|c| c == ':').unwrap()+1..line.chars().position(|c| c == '|').unwrap()]).trim().split_whitespace().collect();
+        let my_nums: Vec<&str> = (&line[line.chars().position(|c| c == '|').unwrap()+1..line.len()]).trim().split_whitespace().collect();
+
+        let mut matches: u32 = 0;
+
+        for num in my_nums {
+            if winning_nums.contains(&num) {
+                matches += 1;
+            }
+        }
+        for _i in 0..copies[index] {
+            total += 1;
+            for i in 1..matches+1 {
+                copies[index + i as usize] += 1;
+            }
+        }
+    }
+    println!("Total scratchcards: {total}");
 }
 
 
