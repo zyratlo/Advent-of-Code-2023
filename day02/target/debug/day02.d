8d64903b5b58aa02115fa5b55db34005ed9aca5e
Ryan Zhang <zyratlo@gmail.com>
2023-12-04 18:32:23 -0800

    part 1 solution

---
+++ day02/target/debug/day02.d
@@ -0,0 +1,1 @@
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\day02.exe: C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\src\main.rs
---
+++ day02/target/debug/deps/day02.d
@@ -0,0 +1,5 @@
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\deps\day02.exe: src\main.rs
+
+C:\Users\zyrat\Documents\Advent-of-Code-2023\day02\target\debug\deps\day02.d: src\main.rs
+
+src\main.rs:
